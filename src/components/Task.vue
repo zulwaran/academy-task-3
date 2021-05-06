e4c304b4dbdf39e8e5e56fb01abe9e83645f86ac
zulwaran <81923601+zulwaran@users.noreply.github.com>
2021-05-07 03:59:46 +0700

    Update task completed for firebase

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -13,6 +13,9 @@
 </template>
 
 <script>
+import firebase from "firebase/app";
+import "firebase/firestore";
+import { db } from "../firebase";
 export default {
   name: "Task",
   props: {
@@ -20,46 +23,34 @@ export default {
   },
   methods: {
     async onDelete(id) {
-      //Удаляем задачу
       if (confirm("Вы действительно хотите удалить задачу?")) {
-        const ref = await fetch(process.env.VUE_DB_URL + `/tasks/${id}`, {
-          method: "DELETE",
+        const tasksRef = await db.collection("tasks");
+        let snapshot = await tasksRef.where("id", "==", id).get();
+        await snapshot.forEach((doc) => {
+          const deletedTask = doc.id;
+          db.collection("tasks").doc(deletedTask).delete();
         });
-        if (ref.status === 200) {
-          this.$store.state.tasks = this.$store.state.tasks.filter(
-            (task) => task.id !== id
-          );
-          this.$store.state.currentTasks = this.$store.state.currentTasks.filter(
-            (task) => task.id !== id
-          );
-        } else {
-          alert("Error");
-        }
+        this.$store.state.tasks = await this.$store.state.tasks.filter(
+          (task) => task.id !== id
+        );
+        this.$store.state.currentTasks = this.$store.state.currentTasks.filter(
+          (task) => task.id !== id
+        );
       }
 
-      //Уменьшаем количество задач в списке дел
-      const currentId = this.$store.state.currentList.id;
-      const list = await fetch(process.env.VUE_DB_URL + `/lists/${currentId}`);
-      const dataList = await list.json();
-      const updList = { ...dataList, count_tasks: dataList.count_tasks - 1 };
-      const res = await fetch(process.env.VUE_DB_URL + `/lists/${currentId}`, {
-        method: "PUT",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(updList),
+      const listsRef = await db.collection("lists");
+      let snapshot = await listsRef
+        .where("id", "==", this.$store.state.currentList.id)
+        .get();
+      await snapshot.forEach((doc) => {
+        const decListId = doc.id;
+        var decrementList = db.collection("lists").doc(decListId);
+        decrementList.update({
+          count_tasks: firebase.firestore.FieldValue.increment(-1),
+        });
       });
-      const data = await res.json();
-      this.$store.state.lists = this.$store.state.lists.map((list) =>
-        list.id === data.id ? { ...list, count_tasks: data.count_tasks } : list
-      );
-      this.$store.state.visibleLists = this.$store.state.visibleLists.map(
-        (list) =>
-          list.id === data.id
-            ? { ...list, count_tasks: data.count_tasks }
-            : list
-      );
 
+      /*  
       //Отображаем статус списков дел
       for (let list in this.$store.state.visibleLists) {
         let count = 0;
@@ -80,27 +71,26 @@ export default {
           this.$store.state.visibleLists[list].color = "green";
         }
       }
-      return data;
+      return data; */
     },
 
     async taskCompleted(id) {
-      //Меняем статус задачи
-      const task = await fetch(process.env.VUE_DB_URL + `/tasks/${id}`);
-      const dataTask = await task.json();
-      const updTask = { ...dataTask, completed: !dataTask.completed };
-      const res = await fetch(process.env.VUE_DB_URL + `/tasks/${id}`, {
-        method: "PUT",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(updTask),
+      const taskRef = await db.collection("tasks");
+      let taskSnapshot = await taskRef.where("id", "==", id).get();
+      await taskSnapshot.forEach((doc) => {
+        var taskCheck = db.collection("tasks").doc(doc.id);
+        if (doc.data().completed === true) {
+          return taskCheck.update({
+            completed: false,
+          });
+        } else {
+          return taskCheck.update({
+            completed: true,
+          });
+        }
       });
-      const data = await res.json();
-      this.$store.state.tasks = this.$store.state.tasks.map((task) =>
-        task.id === data.id ? { ...task, completed: !task.completed } : task
-      );
 
-      //Обновляем статусы задач
+      /*    //Обновляем статусы задач
       for (let list in this.$store.state.visibleLists) {
         let count = 0;
         for (let task in this.$store.state.tasks) {
@@ -119,7 +109,7 @@ export default {
         } else {
           this.$store.state.visibleLists[list].color = "green";
         }
-      }
+      }*/
     },
   },
 };
