7184059105c5c011718cbec4558be1f203609612
zulwaran <81923601+zulwaran@users.noreply.github.com>
2021-05-07 11:01:00 +0700

    fixed a bug of adding a task

--- src/components/AddList.vue
+++ src/components/AddList.vue
@@ -24,28 +24,34 @@ export default {
         return;
       }
 
-      await db.collection("lists").add({
+      const newList = {
         id: Math.floor(Math.random() * 100000),
         uid: this.$store.state.uid,
         name: text,
         count_tasks: 0,
         color: "",
-      });
+      };
 
-      const listsRef = await db.collection("lists");
-      let snapshot = await listsRef
-        .where("uid", "==", this.$store.state.uid)
-        .get();
-      await snapshot.forEach((doc) => {
-        let docList = doc.data();
-        this.$store.state.lists.push(docList);
-      });
-      this.$store.state.lists.sort((a, b) => a.name.localeCompare(b.name));
-      this.$store.state.visibleLists = this.$store.state.lists;
-
+      await db
+        .collection("lists")
+        .doc()
+        .set({
+          name: newList.name,
+          id: newList.id,
+          uid: newList.uid,
+          count_tasks: newList.count_tasks,
+          color: newList.color,
+        })
+        .then(() => {
+          this.$store.state.lists = [...this.$store.state.lists, newList];
+          this.$store.state.lists.sort((a, b) => a.name.localeCompare(b.name));
+        });
       this.text = "";
+      this.filter()
+    },
 
-      //Осуществляем фильтрацию и вывод на экран нового списка дел
+    //Осуществляем фильтрацию и вывод на экран нового списка дел
+    async filter() {
       if (this.$store.state.selectedOption == "all") {
         this.$store.state.visibleLists = this.$store.state.lists;
       }
