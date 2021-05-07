dd0a1929477dc6510392aaf1c6df8f2b43846656
zulwaran <81923601+zulwaran@users.noreply.github.com>
2021-05-07 11:30:38 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,17 @@ import firebase from 'firebase/app'
 import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAYDMajV7G4NupPZzeXLU4d3dkc8M50UqQ",
-    authDomain: "academy-task-3.firebaseapp.com",
-    databaseURL: "https://academy-task-3-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "academy-task-3",
-    storageBucket: "academy-task-3.appspot.com",
-    messagingSenderId: "1062326428640",
-    appId: "1:1062326428640:web:9cb3748eb4fc6690348185"
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_PROJECT_ID + ".firebaseapp.com",
+    databaseURL: "https://" + process.env.VUE_APP_FIREBASE_PROJECT_ID + "-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_PROJECT_ID + ".appspot.com",
+    messagingSenderId: process.env.VUE_APP_FIREBASE_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
+
 
 export const db = firebaseApp.firestore()
 const usersCollection = db.collection('users')
